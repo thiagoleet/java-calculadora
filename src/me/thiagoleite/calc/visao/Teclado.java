e9ae50c20daff53fc28332cca093df98ef980870
Thiago Leite <thiagofmleite@gmail.com>
2023-08-20 15:01:19 -0300

    Iniciando com o layout do Teclado

--- src/me/thiagoleite/calc/visao/Teclado.java
+++ src/me/thiagoleite/calc/visao/Teclado.java
@@ -1,7 +1,8 @@
 package me.thiagoleite.calc.visao;
 
 import javax.swing.JPanel;
-import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.awt.Color;
 
 
@@ -11,8 +12,50 @@ public class Teclado extends JPanel {
     private final Color COR_LARANJA = new Color(242, 163, 60);
 
     public Teclado() {
-        setLayout(new GridLayout(5, 4));
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+
+        setLayout(layout);
+
+        // Linha 1
+        adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
+        adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
+        adicionarBotao("%", COR_CINZA_ESCURO, c, 2, 0);
+        adicionarBotao("/", COR_LARANJA, c, 3, 0);
+
+        // Linha 2
+        adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
+        adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
+        adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
+        adicionarBotao("*", COR_LARANJA, c, 3, 1);
+
+        // Linha 3
+        adicionarBotao("4", COR_CINZA_CLARO, c, 0, 2);
+        adicionarBotao("5", COR_CINZA_CLARO, c, 1, 2);
+        adicionarBotao("6", COR_CINZA_CLARO, c, 2, 2);
+        adicionarBotao("-", COR_LARANJA, c, 3, 2);
+
+        // Linha 4
+        adicionarBotao("1", COR_CINZA_CLARO, c, 0, 3);
+        adicionarBotao("2", COR_CINZA_CLARO, c, 1, 3);
+        adicionarBotao("3", COR_CINZA_CLARO, c, 2, 3);
+        adicionarBotao("+", COR_LARANJA, c, 3, 3);
+
+        // Linha 5
+        adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
+        adicionarBotao("0", COR_CINZA_CLARO, c, 1, 4);
+        adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
+        adicionarBotao("=", COR_LARANJA, c, 3, 4);
 
 
     }
+
+    private void adicionarBotao(String texto, Color cor, GridBagConstraints c, int x, int y) {
+        c.gridx = x;
+        c.gridy = y;
+        Botao botao = new Botao(texto, cor);
+
+        add(botao, c);
+    }
+
 }
